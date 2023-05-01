67c1c32e2290f8c88c51a9bf9dcf111f08d98a56
Shubham73-dev <79575532+Shubham73-dev@users.noreply.github.com>
2023-05-01 17:25:20 +0530

    Add files via upload

---
+++ CSS-3/sports-365-flex/scripts/learning.js
@@ -0,0 +1,10 @@
+// there are following ways to access DOM elemnts:
+// 1. document.getElementById('');
+// 2. document.getElementsByClassName('')[i];
+// 3.document.getElementsByTagName('');
+// 4.document.querySelector('');
+// 5.document.querySelectorAll('');
+// var bangaloreDiv = document.getElementsByClassName('bangaloreDiv')[0];
+// console.log(bangaloreDiv);
+// var Pagewrapper = document.getElementById('page_wrapper')
+// console.log('pagewrapper value -->',Pagewrapper);
